92e7cb4bf4d142ef9040de75405e38a2058dfb72
qiaouchicago <68757394+qiaouchicago@users.noreply.github.com>
2022-08-17 14:12:14 -0500

    DEV 1323 add validator models (#2)
    * add validator models
    
    * move more validator tests
    
    * move test_json_validator_with_array_prop
    
    * move test_json_validators.py
    
    * refactor test_json_validators.py
    
    * add test_graph_validator.py
    
    * add test_graph_validator_with_wrong_multiplicity
    
    * add test_graph_validator_with_correct_node
    
    * add test_graph_validator_with_existing_unique_keys
    
    * add test_graph_validator_with_existing_unique_keys_for_different_node_types
    
    * add validators to extras_require
    
    * change focal to bionic for travis to avoid postgres error

---
+++ tests/integration/validators/test_graph_validator.py
@@ -0,0 +1,285 @@
+import uuid
+
+from gdcdatamodel2 import models, validators
+from tests import MockSubmissionEntity
+
+
+def create_node(gdc_graph, doc, session):
+    cls = models.Node.get_subclass(doc["type"])
+    node = cls(str(uuid.uuid4()))
+    node.props = doc["props"]
+    for key, value in doc["edges"].items():
+        for target_id in value:
+            edge = gdc_graph.nodes().ids(target_id).first()
+            node[key].append(edge)
+    session.add(node)
+    return node
+
+
+def update_schema(graph_validator, entity, key, schema):
+    graph_validator.schemas.schema[entity][key] = schema
+
+
+def test_graph_validator_without_required_link(gdc_graph):
+    graph_validator = validators.GDCGraphValidator()
+    entities = [MockSubmissionEntity()]
+
+    with gdc_graph.session_scope() as session:
+        node = create_node(
+            gdc_graph,
+            {"type": "aliquot", "props": {"submitter_id": "test"}, "edges": {}},
+            session,
+        )
+        entities[0].node = node
+        update_schema(
+            graph_validator,
+            "aliquot",
+            "links",
+            [
+                {
+                    "name": "analytes",
+                    "backref": "aliquots",
+                    "label": "derived_from",
+                    "multiplicity": "many_to_one",
+                    "target_type": "analyte",
+                    "required": True,
+                }
+            ],
+        )
+        graph_validator.record_errors(gdc_graph, entities)
+        assert entities[0].errors[0]["keys"] == ["analytes"]
+
+
+def test_graph_validator_with_exclusive_link(gdc_graph):
+    graph_validator = validators.GDCGraphValidator()
+    entities = [MockSubmissionEntity()]
+
+    with gdc_graph.session_scope() as session:
+        analyte = create_node(
+            gdc_graph,
+            {
+                "type": "analyte",
+                "props": {"submitter_id": "test", "analyte_type_id": "D", "analyte_type": "DNA"},
+                "edges": {},
+            },
+            session,
+        )
+        sample = create_node(
+            gdc_graph,
+            {
+                "type": "sample",
+                "props": {"submitter_id": "test", "sample_type": "DNA", "sample_type_id": "01"},
+                "edges": {},
+            },
+            session,
+        )
+
+        node = create_node(
+            gdc_graph,
+            {
+                "type": "aliquot",
+                "props": {"submitter_id": "test"},
+                "edges": {"analytes": [analyte.node_id], "samples": [sample.node_id]},
+            },
+            session,
+        )
+        entities[0].node = node
+        update_schema(
+            graph_validator,
+            "aliquot",
+            "links",
+            [
+                {
+                    "exclusive": True,
+                    "required": True,
+                    "subgroup": [
+                        {
+                            "name": "analytes",
+                            "backref": "aliquots",
+                            "label": "derived_from",
+                            "multiplicity": "many_to_one",
+                            "target_type": "analyte",
+                        },
+                        {
+                            "name": "samples",
+                            "backref": "aliquots",
+                            "label": "derived_from",
+                            "multiplicity": "many_to_one",
+                            "target_type": "sample",
+                        },
+                    ],
+                }
+            ],
+        )
+        graph_validator.record_errors(gdc_graph, entities)
+        assert entities[0].errors[0]["keys"] == ["analytes", "samples"]
+
+
+def test_graph_validator_with_wrong_multiplicity(gdc_graph):
+    graph_validator = validators.GDCGraphValidator()
+    entities = [MockSubmissionEntity()]
+
+    with gdc_graph.session_scope() as session:
+        analyte = create_node(
+            gdc_graph,
+            {
+                "type": "analyte",
+                "props": {"submitter_id": "test", "analyte_type_id": "D", "analyte_type": "DNA"},
+                "edges": {},
+            },
+            session,
+        )
+
+        analyte_b = create_node(
+            gdc_graph,
+            {
+                "type": "analyte",
+                "props": {"submitter_id": "testb", "analyte_type_id": "H", "analyte_type": "RNA"},
+                "edges": {},
+            },
+            session,
+        )
+
+        node = create_node(
+            gdc_graph,
+            {
+                "type": "aliquot",
+                "props": {"submitter_id": "test"},
+                "edges": {"analytes": [analyte.node_id, analyte_b.node_id]},
+            },
+            session,
+        )
+        entities[0].node = node
+        update_schema(
+            graph_validator,
+            "aliquot",
+            "links",
+            [
+                {
+                    "exclusive": False,
+                    "required": True,
+                    "subgroup": [
+                        {
+                            "name": "analytes",
+                            "backref": "aliquots",
+                            "label": "derived_from",
+                            "multiplicity": "many_to_one",
+                            "target_type": "analyte",
+                        },
+                        {
+                            "name": "samples",
+                            "backref": "aliquots",
+                            "label": "derived_from",
+                            "multiplicity": "many_to_one",
+                            "target_type": "sample",
+                        },
+                    ],
+                }
+            ],
+        )
+        graph_validator.record_errors(gdc_graph, entities)
+        assert entities[0].errors[0]["keys"] == ["analytes"]
+
+
+def test_graph_validator_with_correct_node(gdc_graph):
+    graph_validator = validators.GDCGraphValidator()
+    entities = [MockSubmissionEntity()]
+
+    with gdc_graph.session_scope() as session:
+        analyte = create_node(
+            gdc_graph,
+            {
+                "type": "analyte",
+                "props": {"submitter_id": "test", "analyte_type_id": "D", "analyte_type": "DNA"},
+                "edges": {},
+            },
+            session,
+        )
+
+        node = create_node(
+            gdc_graph,
+            {
+                "type": "aliquot",
+                "props": {"submitter_id": "test"},
+                "edges": {"analytes": [analyte.node_id]},
+            },
+            session,
+        )
+        entities[0].node = node
+        update_schema(
+            graph_validator,
+            "aliquot",
+            "links",
+            [
+                {
+                    "exclusive": False,
+                    "required": True,
+                    "subgroup": [
+                        {
+                            "name": "analytes",
+                            "backref": "aliquots",
+                            "label": "derived_from",
+                            "multiplicity": "many_to_one",
+                            "target_type": "analyte",
+                        },
+                        {
+                            "name": "samples",
+                            "backref": "aliquots",
+                            "label": "derived_from",
+                            "multiplicity": "many_to_one",
+                            "target_type": "sample",
+                        },
+                    ],
+                }
+            ],
+        )
+        graph_validator.record_errors(gdc_graph, entities)
+        assert len(entities[0].errors) == 0
+
+
+def test_graph_validator_with_existing_unique_keys(gdc_graph):
+    graph_validator = validators.GDCGraphValidator()
+    entities = [MockSubmissionEntity()]
+
+    with gdc_graph.session_scope() as session:
+        node = create_node(
+            gdc_graph, {"type": "data_format", "props": {"name": "test"}, "edges": {}}, session
+        )
+        node = create_node(
+            gdc_graph, {"type": "data_format", "props": {"name": "test"}, "edges": {}}, session
+        )
+        update_schema(graph_validator, "data_format", "uniqueKeys", [["name"]])
+        entities[0].node = node
+        graph_validator.record_errors(gdc_graph, entities)
+        assert entities[0].errors[0]["keys"] == ["name"]
+
+
+def test_graph_validator_with_existing_unique_keys_for_different_node_types(gdc_graph):
+    graph_validator = validators.GDCGraphValidator()
+    entities = [MockSubmissionEntity()]
+
+    with gdc_graph.session_scope() as session:
+        node = create_node(
+            gdc_graph,
+            {"type": "sample", "props": {"submitter_id": "test", "project_id": "A"}, "edges": {}},
+            session,
+        )
+        node = create_node(
+            gdc_graph,
+            {
+                "type": "aliquot",
+                "props": {"submitter_id": "test", "project_id": "A"},
+                "edges": {},
+            },
+            session,
+        )
+        update_schema(
+            graph_validator, "data_format", "uniqueKeys", [["submitter_id", "project_id"]]
+        )
+        entities[0].node = node
+        graph_validator.record_errors(gdc_graph, entities)
+        error_keys = {tuple(sorted(e["keys"])) for e in entities[0].errors}
+        # Check (project_id, submitter_id) uniqueness is captured
+        assert ("project_id", "submitter_id") in error_keys
+        # Check that missing edges is captured
+        assert ("analytes", "samples") in error_keys
---
+++ tests/unit/validators/test_json_validator.py
@@ -0,0 +1,111 @@
+from copy import copy
+
+from gdcdatamodel2 import validators
+from tests import MockSubmissionEntity
+
+
+def test_json_validator_with_insufficient_properties():
+    entities = [MockSubmissionEntity()]
+    json_validator = validators.GDCJSONValidator()
+    entities[0].doc = {"type": "aliquot", "centers": {"submitter_id": "test"}}
+    json_validator.record_errors(entities)
+    assert entities[0].errors[0]["keys"] == ["submitter_id"]
+    assert len(entities[0].errors) == 1
+
+
+def test_json_validator_with_wrong_node_type():
+    entities = [MockSubmissionEntity()]
+    json_validator = validators.GDCJSONValidator()
+    entities[0].doc = {"type": "aliquo"}
+    json_validator.record_errors(entities)
+    assert entities[0].errors[0]["keys"] == ["type"]
+    assert len(entities[0].errors) == 1
+
+
+def test_json_validator_with_wrong_property_type():
+    entities = [MockSubmissionEntity()]
+    json_validator = validators.GDCJSONValidator()
+    entities[0].doc = {"type": "aliquot", "submitter_id": 1, "centers": {"submitter_id": "test"}}
+    json_validator.record_errors(entities)
+    assert entities[0].errors[0]["keys"] == ["submitter_id"]
+    assert len(entities[0].errors) == 1
+
+
+def test_json_validator_with_multiple_errors():
+    entities = [MockSubmissionEntity()]
+    json_validator = validators.GDCJSONValidator()
+    entities[0].doc = {
+        "type": "aliquot",
+        "submitter_id": 1,
+        "test": "test",
+        "centers": {"submitter_id": "test"},
+    }
+    json_validator.record_errors(entities)
+    assert len(entities[0].errors) == 2
+
+
+def test_json_validator_with_nested_error_keys():
+    entities = [MockSubmissionEntity()]
+    json_validator = validators.GDCJSONValidator()
+    entities[0].doc = {
+        "type": "aliquot",
+        "submitter_id": "test",
+        "centers": {"submitter_id": True},
+    }
+    json_validator.record_errors(entities)
+    assert entities[0].errors[0]["keys"] == ["centers"]
+
+
+def test_json_validator_with_multiple_entities():
+    entities = [MockSubmissionEntity()]
+    json_validator = validators.GDCJSONValidator()
+    entities[0].doc = {
+        "type": "aliquot",
+        "submitter_id": 1,
+        "test": "test",
+        "centers": {"submitter_id": "test"},
+    }
+    entity = MockSubmissionEntity()
+    entity.doc = {"type": "aliquot", "submitter_id": "test", "centers": {"submitter_id": "test"}}
+    entities.append(entity)
+
+    json_validator.record_errors(entities)
+    assert len(entities[0].errors) == 2
+    assert len(entity.errors) == 0
+
+
+def test_json_validator_with_array_prop():
+    entity_doc = {
+        "type": "diagnosis",
+        "submitter_id": "test",
+        "age_at_diagnosis": 10,
+        "primary_diagnosis": "Abdominal desmoid",
+        "morphology": "8000/0",
+        "tissue_or_organ_of_origin": "Abdomen, NOS",
+        "site_of_resection_or_biopsy": "Abdomen, NOS",
+    }
+
+    def mock_doc(sites_of_involvement):
+        mock = MockSubmissionEntity()
+        mock.doc = copy(entity_doc)
+        mock.doc["sites_of_involvement"] = sites_of_involvement
+        return mock
+
+    # Right is invalid value and diagnosis_is_primary_disease is required
+    entities = [
+        mock_doc(["Right"]),
+        mock_doc(["Cervix", "Right"]),
+        mock_doc(["Cervix", "Ovary, NOS"]),
+    ]
+
+    json_validator = validators.GDCJSONValidator()
+
+    json_validator.record_errors(entities)
+    assert len(entities[0].errors) == 2
+    assert len(entities[1].errors) == 2
+    assert len(entities[2].errors) == 1
+    error_keys_zero = sorted(error["keys"][0] for error in entities[0].errors)
+    error_keys_one = sorted(error["keys"][0] for error in entities[1].errors)
+
+    assert error_keys_zero == ["diagnosis_is_primary_disease", "sites_of_involvement.0"]
+    assert error_keys_one == ["diagnosis_is_primary_disease", "sites_of_involvement.1"]
