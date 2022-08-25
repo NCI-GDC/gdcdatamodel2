3d014f1e87d21cd5b1e165d5dc3b5a8aeecf419c
Qiao Qiao <qiaoq@uchicago.edu>
2022-08-25 14:43:16 -0500

    import versioned_nodes in init

--- src/gdcdatamodel2/models/__init__.py
+++ src/gdcdatamodel2/models/__init__.py
@@ -302,7 +302,7 @@ from .germlinemutationcallingworkflowperformedonsubmittedgenotypingarray import 
 from .germlinemutationcallingworkflowrelatestocase import (
     GermlineMutationCallingWorkflowRelatesToCase,
 )
-from .helpers import base, indexes, related_cases, versioning
+from .helpers import base, indexes, related_cases, versioned_nodes, versioning
 from .masked_methylation_array import MaskedMethylationArray
 from .masked_somatic_mutation import MaskedSomaticMutation
 from .maskedmethylationarraydatafrommethylationarrayharmonizationworkflow import (
