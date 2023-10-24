f46880e4525a9f97467614586bd9f1c77deade69
Qiao Qiao <qiaoq@uchicago.edu>
2023-10-24 09:14:37 -0500

    update black version

--- src/gdcdatamodel2/models/helpers/versioned_nodes.py
+++ src/gdcdatamodel2/models/helpers/versioned_nodes.py
@@ -12,7 +12,6 @@ T = TypeVar("T", bound="VersionedNode")
 
 
 class VersionedNode(Base):
-
     __tablename__ = "versioned_nodes"
     __table_args__ = (
         Index("submitted_node_id_idx", "node_id"),
