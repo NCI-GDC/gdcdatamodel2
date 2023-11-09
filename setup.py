eb1d1d66f05d841a78c3e681b7ac00e5e0433577
Qiao Qiao <qiaoq@uchicago.edu>
2023-11-09 09:23:29 -0600

    update versionning

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+import re
+
+from setuptools import setup
+from setuptools_scm import Configuration, _do_parse
+
+
+def get_version():
+    config = Configuration()
+    version = _do_parse(config)
+    if version.distance == 0:
+        return str(version.tag)
+
+    from setuptools_scm.version import guess_next_version
+
+    branch = os.getenv("CI_COMMIT_REF_NAME", version.branch)
+    if branch.startswith("release"):
+        fmt = "{guessed}rc{distance}"
+    elif branch.startswith("develop"):
+        fmt = "{guessed}a{distance}"
+    else:
+        local = re.sub("[^0-9a-zA-Z]+", ".", branch)
+        fmt = f"{{guessed}}.dev{{distance}}+{local}"
+
+    return version.format_next_version(guess_next_version, fmt)
+
+
+setup(
+    setuptools_git_versioning={
+        "enabled": True,
+        "version_callback": get_version,
+    },
+)
