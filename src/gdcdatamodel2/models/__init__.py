b02f37292d9c8ed56bc1eb8b73b97f236908ac6a
Gitlab CI <git@gitlab.datacommons.io>
2024-04-24 21:06:16 +0000

    Use gdcdictionary develop f6f10fa265335d163276d97460e646cdabadb168

--- src/gdcdatamodel2/models/__init__.py
+++ src/gdcdatamodel2/models/__init__.py
@@ -120,6 +120,9 @@ from .annotationannotatesslideimage import AnnotationAnnotatesSlideImage
 from .annotationannotatessomaticmutationindex import AnnotationAnnotatesSomaticMutationIndex
 from .annotationannotatesstructuralvariation import AnnotationAnnotatesStructuralVariation
 from .annotationannotatessubmittedalignedreads import AnnotationAnnotatesSubmittedAlignedReads
+from .annotationannotatessubmittedexpressionarray import (
+    AnnotationAnnotatesSubmittedExpressionArray,
+)
 from .annotationannotatessubmittedgenomicprofile import AnnotationAnnotatesSubmittedGenomicProfile
 from .annotationannotatessubmittedgenotypingarray import (
     AnnotationAnnotatesSubmittedGenotypingArray,
@@ -461,6 +464,7 @@ from .structuralvariationdatafromstructuralvariantcallingworkflow import (
 )
 from .structuralvariationrelatestocase import StructuralVariationRelatesToCase
 from .submitted_aligned_reads import SubmittedAlignedReads
+from .submitted_expression_array import SubmittedExpressionArray
 from .submitted_genomic_profile import SubmittedGenomicProfile
 from .submitted_genotyping_array import SubmittedGenotypingArray
 from .submitted_methylation_beta_value import SubmittedMethylationBetaValue
@@ -468,6 +472,8 @@ from .submitted_tangent_copy_number import SubmittedTangentCopyNumber
 from .submitted_unaligned_reads import SubmittedUnalignedReads
 from .submittedalignedreadsdatafromreadgroup import SubmittedAlignedReadsDataFromReadGroup
 from .submittedalignedreadsrelatestocase import SubmittedAlignedReadsRelatesToCase
+from .submittedexpressionarrayderivedfromaliquot import SubmittedExpressionArrayDerivedFromAliquot
+from .submittedexpressionarrayrelatestocase import SubmittedExpressionArrayRelatesToCase
 from .submittedgenomicprofiledatafromreadgroup import SubmittedGenomicProfileDataFromReadGroup
 from .submittedgenomicprofilerelatestocase import SubmittedGenomicProfileRelatesToCase
 from .submittedgenotypingarrayderivedfromaliquot import SubmittedGenotypingArrayDerivedFromAliquot
@@ -581,6 +587,7 @@ __all__ = [
     "AnnotationAnnotatesSomaticMutationIndex",
     "AnnotationAnnotatesStructuralVariation",
     "AnnotationAnnotatesSubmittedAlignedReads",
+    "AnnotationAnnotatesSubmittedExpressionArray",
     "AnnotationAnnotatesSubmittedGenomicProfile",
     "AnnotationAnnotatesSubmittedGenotypingArray",
     "AnnotationAnnotatesSubmittedMethylationBetaValue",
@@ -817,6 +824,7 @@ __all__ = [
     "StructuralVariationDataFromStructuralVariantCallingWorkflow",
     "StructuralVariationRelatesToCase",
     "SubmittedAlignedReads",
+    "SubmittedExpressionArray",
     "SubmittedGenomicProfile",
     "SubmittedGenotypingArray",
     "SubmittedMethylationBetaValue",
@@ -824,6 +832,8 @@ __all__ = [
     "SubmittedUnalignedReads",
     "SubmittedAlignedReadsDataFromReadGroup",
     "SubmittedAlignedReadsRelatesToCase",
+    "SubmittedExpressionArrayDerivedFromAliquot",
+    "SubmittedExpressionArrayRelatesToCase",
     "SubmittedGenomicProfileDataFromReadGroup",
     "SubmittedGenomicProfileRelatesToCase",
     "SubmittedGenotypingArrayDerivedFromAliquot",
