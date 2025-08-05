11cf50a6f7928de991ba3525eff8bd613ce51e38
Jason Stiles <99777116+stilesj-uchicago@users.noreply.github.com>
2025-08-05 07:39:42 -0400

    Print all the keys in the environment to see if we can find a trigger

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -19,7 +19,8 @@ SAMPLE_PROJECT = "MISC"
 
 @pytest.fixture(scope="session")
 def pg_container() -> typing.Generator[sqlalchemy.engine.Engine | None, None, None]:
-    print("CI_JOB_ID", os.getenv("CI_JOB_ID"))
+    for key in sorted(os.environ):
+        print(f"{key}={os.environ[key]}")
     if os.getenv("CI_JOB_ID"):
         # disable test containers in gitlab ci
         yield None
